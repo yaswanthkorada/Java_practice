6e04cad16b40582b78da7144a8c7ae0e461f771f
Yaswanth Korada <113379084+yaswanthkorada@users.noreply.github.com>
2023-09-08 22:49:34 +0530

    Add files via upload

---
+++ 3 Strings/4 StringBuffer/stri.java
@@ -0,0 +1,7 @@
+//string buffer is a class which is used to cerat mutable string objects.the initial capacity of string buffer is 16.
+public class stri {
+public static void main(String[] args) {
+    StringBuffer s1 = new StringBuffer("Teja");
+    System.out.println(s1);
+}
+}
